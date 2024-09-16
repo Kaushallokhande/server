464babe31292e015418640f500f4fa701e182803
Kaushal <kaushallokhande12345@gamil.com>
2024-09-16 20:45:51 +0530

    deploy

--- index.js
+++ index.js
@@ -11,22 +11,21 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
-
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*', methods: 'GET,POST' }));
 app.use(bodyParser.json());
+
+app.use("/", chatroutes);
 
 app.use((err, req, res, next) => {
   if (err.message.includes('GoogleGenerativeAIResponseError')) {
-      console.error('Google AI Service Error:', err);
-      res.status(500).json({ message: 'There was an error with the Google AI service. Please try again later.' });
+    console.error('Google AI Service Error:', err);
+    res.status(500).json({ message: 'There was an error with the Google AI service. Please try again later.' });
   } else {
-      console.error('Server Error:', err);
-      res.status(500).json({ message: 'An unexpected error occurred. Please try again later.' });
+    console.error('Server Error:', err);
+    res.status(500).json({ message: 'An unexpected error occurred. Please try again later.' });
   }
 });
-app.use("/", chatroutes);
 
 const PORT = process.env.PORT || 5050;
-
-app.listen(PORT, () => console.log(`Server on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
