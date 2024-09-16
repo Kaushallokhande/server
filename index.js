00b6b8c81b4c94ad63effbd35adfd37c5f408ae5
Kaushal <kaushallokhande12345@gamil.com>
2024-09-16 20:34:33 +0530

    fix deploy

--- index.js
+++ index.js
@@ -7,15 +7,13 @@ const mongoose = require("mongoose");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
-const app = express();
-app.use(cors({ origin: 'https://chatpage-self.vercel.app' }));
 
+const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use((err, req, res, next) => {
